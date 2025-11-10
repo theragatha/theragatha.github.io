d313e3dac0a8195e30ed422b2433bde2abe980f7
puthujjana <puthujjana@users.noreply.github.com>
2025-11-10 20:23:40 +0000

    deploy: 74947eed200435e571c5719bedbb144c71059df8

--- en/theragatha/thanissaro/chapter-three/gen.py
+++ en/theragatha/thanissaro/chapter-three/gen.py
@@ -2,11 +2,11 @@ import os
 
 def gen(verse_number):
         new_front_matter = f"""---
-title: "2.{verse_number}"
-id: "thag2.{verse_number}"
-chapter: 2
+title: "3.{verse_number}"
+id: "thag3.{verse_number}"
+chapter: 3
 verse: verse_number
-slug: "thag2.{verse_number}"
+slug: "thag3.{verse_number}"
 edition: "Dhammatalks.org"
 collection: "Theragāthā"
 pali_source: "Dhammatalks.org"
@@ -16,10 +16,10 @@ bookHidden: true
 ---
 """
 
-        new_content = new_front_matter + "\n# 2." + verse_number + "\n\n## Notes"
+        new_content = new_front_matter + "\n# 3." + verse_number + "\n\n## Notes"
 
         # Write the new file (e.g., thag1.4.md)
-        output_filename = f"thag2.{verse_number}.md"
+        output_filename = f"thag3.{verse_number}.md"
         with open(output_filename, "w", encoding="utf-8") as out:
             out.write(new_content)
         print(f"Wrote {output_filename}")
