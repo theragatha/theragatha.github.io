48390d5eca6a17163b604df3ee1efdb75afd6e1b
puthujjana <puthujjana@users.noreply.github.com>
2025-11-20 22:48:49 +0000

    deploy: f81ac62bca8d290056142009d7469b491a428aa9

---
+++ es/theragatha/bosque-theravada/gen.py
@@ -0,0 +1,30 @@
+import os
+
+chapter_number = "1"
+
+def gen(verse_number):
+        new_front_matter = f"""---
+title: "{chapter_number}.{verse_number}"
+id: "thag{chapter_number}.{verse_number}"
+chapter: {chapter_number}
+verse: verse_number
+slug: "thag{chapter_number}.{verse_number}"
+edition: "Dhammatalks.org"
+collection: "Theragāthā"
+pali_source: "Dhammatalks.org"
+translator: "Ṭhānissaro Bhikkhu"
+weight: {verse_number}
+bookHidden: true
+---
+"""
+
+        new_content = new_front_matter + "\n# " + chapter_number + "." + verse_number + "\n\n## Notes"
+
+        # Write the new file (e.g., thag1.4.md)
+        output_filename = f"thag{chapter_number}.{verse_number}.md"
+        with open(output_filename, "w", encoding="utf-8") as out:
+            out.write(new_content)
+        print(f"Wrote {output_filename}")
+
+vnum = input("Enter the verse number: ")
+gen(vnum)
